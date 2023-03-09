df900133dbbb100940f7cd2b96a915d9242197e6
nmill245 <nmiller45@outlook.com>
2023-03-09 17:55:33 -0600

    Got js to read from json and add to html. Updated HTML titles.

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 fetch('./data.json')
-    .then(function(response) {return response.json})
-    .then(function(data) { getData(data); })
+    .then(function(response) {return response.json()})
+    .then(function (data) { getData(data); })
     .catch(function (err) {console.log('error' + err);})
+
 function getData(data){
-    let i = 0;
+    let i = 1;
         for(let productName in data){
             if(productName == "Mens Shoes"){
-
+                for(let element of data[productName]){
+                    let outer = document.getElementById(String(i));
+                    let text = document.createElement("p");
+                    text.className = "card-text";
+                    text.innerHTML = `${element["desc"]} <br> Price: $${element["price"]}`
+                    outer.appendChild(text);
+                    i++;
+                }
             }
         }
 }
