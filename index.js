7e3cc552809e32c555c425dd1aa15edb7e58ffba
nmill245 <nmiller45@outlook.com>
2023-03-09 11:06:52 -0600

    attempting to write a code to include desc from json file

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ fetch('./data.json')
     .then(function(response) {return response.json})
     .then(function(data) { getData(data); })
     .catch(function (err) {console.log('error' + err);})
+function getData(data){
+    let i = 0;
+        for(let productName in data){
+            if(productName == "Mens Shoes"){
+
+            }
+        }
+}
