fe53d93771b833c134275c8f3ea0f7f9cca16626
mtgnoah <noahpravecek@gmail.com>
2023-03-10 15:35:17 -0600

    Womens shoes added

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-fetch('./data.json')
-    .then(function(response) {return response.json()})
-    .then(function (data) { getData(data); })
-    .catch(function (err) {console.log('error' + err);})
+function getMenDataFromJson() {
+    fetch('./data.json')
+        .then(function (response) { return response.json() })
+        .then(function (data) { getMensData(data); })
+        .catch(function (err) { console.log('error' + err); })
+    
+}
 
-function getData(data){
+function getWomenDataFromJson() {
+    fetch('./data.json')
+        .then(function (response) { return response.json() })
+        .then(function (data) { getWomensData(data); })
+        .catch(function (err) { console.log('error' + err); })
+}
+
+function getMensData(data){
     let i = 1;
         for(let productName in data){
             if(productName == "Mens Shoes"){
@@ -17,4 +27,20 @@ function getData(data){
                 }
             }
         }
+}
+function getWomensData(data) {
+    let i = 1;
+    for (let productName in data) {
+        console.log("HERE")
+        if (productName == "Womens_Shoes") {
+            for (let element of data[productName]) {
+                let outer = document.getElementById(String(i));
+                let text = document.createElement("p");
+                text.className = "card-text";
+                text.innerHTML = `Product Name: ${element["name"]}<br>${element["desc"]} <br> Price: $${element["price"]}`
+                outer.appendChild(text);
+                i++;
+            }
+        }
+    }
 }
