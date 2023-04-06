4a1efc9aaae5b4248035765d47db34f50a4d93bd
mtgnoah <noahpravecek@gmail.com>
2023-04-06 16:32:21 -0500

    works for react now with images

---
+++ assignment2/src/Products.js
@@ -0,0 +1,45 @@
+import blackconverse from "./images/blackconverse.jpg";
+import BlueLaceUpSneaker from "./images/BlueLaceUpSneaker.jpg";
+import jordans from "./images/jordans.jpg";
+import yeezy350 from "./images/yeezy350.jpg";
+import YellowVans from "./images/YellowVans.jpg";
+
+
+
+
+
+export const Products = [
+    {
+        "image": blackconverse,
+        "price": "125.00",
+        "desc": "Black Converse, a stable in anyone' shoe collection. Commonly known to be worn for skating or if you are from an alternative crowd.",
+        "name": "Black Converse"
+    },
+    {
+        "image": BlueLaceUpSneaker,
+        "price": "100.00",
+        "desc": "These blue lace up sneakers may be a standalone piece, or mesh within an overall outfit of blue.",
+        "name": "Blue Sneakers"
+    },
+
+    {
+        "image": jordans,
+        "price": "150.00",
+        "desc": "Jordan 1's, the beginning of one of the most popular shoes brands in the world, make a stable here. Named after the legendary Michael Jordan these shoes are a great accent piece.",
+        "name": "Jordan 1's"
+    },
+
+    {
+        "image": yeezy350,
+        "price": "150.00",
+        "desc": "Yeezy's, made by popular shoe brand Adidas, compliment ones style while also showing off ones class. These shoes range in many prices but usually end up at the higher end of sales.",
+        "name": "Yeezy"
+    },
+
+    {
+        "image": YellowVans,
+        "price": "125.00",
+        "desc": "Vans, a stable of the skating community, continues it's reign with these yellow shoes. Being flat footed, and with plenty of support for any skating needs, they make a great accent to the board.",
+        "name": "Vans"
+    }
+]
