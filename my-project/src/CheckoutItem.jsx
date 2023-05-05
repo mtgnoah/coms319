7245467432d7e09415c146ada9abb7d2da2298ff
mtgnoah <noahpravecek@gmail.com>
2023-05-05 15:55:06 -0500

    added new files

---
+++ my-project/src/CheckoutItem.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function CheckoutItem({ image, id, name, price, quantity }) {
+
+    const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
+        currency: "USD",
+        style: "currency",
+    })
+
+    function formatCurrency(number) {
+        return CURRENCY_FORMATTER.format(number)
+    }
+
+    return (
+        <li key={id} className="flex py-6">
+            <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                <img
+                    src={image}
+                    className="h-full w-full object-cover object-center"
+                />
+            </div>
+            <div className="ml-4 flex flex-1 flex-col">
+                <div>
+                    <div className="flex justify-between text-base font-medium text-gray-900">
+                        <h3>
+                            {name}
+                        </h3>
+                        <p className="ml-4">{price}</p>
+                    </div>
+                </div>
+                <div className="flex flex-1 items-end justify-between text-sm">
+                    <p className="text-gray-500">Qty {quantity}</p>
+                    <div> {formatCurrency(price * quantity)}</div>
+
+                    <div className="flex">
+                        <button
+                            type="button"
+                            className="font-medium text-indigo-600 hover:text-indigo-500"
+                        >
+                            Remove
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </li>
+    )
+}
+
+export default CheckoutItem
---
+++ my-project/src/NavBar.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useShoppingCart } from "./ShoppingCartContext"
+
+
+function NavBar() {
+    const { openCart, cartQuantity } = useShoppingCart()
+
+    return (
+        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <div className="flex md:order-2">
+                    {/* <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button> */}
+                    <div className="mt-auto">
+                        <button
+                            onClick={openCart}
+                            style={{ width: "3rem", height: "3rem", position: "relative" }}
+                            variant="outline-primary"
+                            className="rounded-circle"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 576 512"
+                                fill="currentColor"
+                            >
+                                <path d="M96 0C107.5 0 117.4 8.19 119.6 19.51L121.1 32H541.8C562.1 32 578.3 52.25 572.6 72.66L518.6 264.7C514.7 278.5 502.1 288 487.8 288H170.7L179.9 336H488C501.3 336 512 346.7 512 360C512 373.3 501.3 384 488 384H159.1C148.5 384 138.6 375.8 136.4 364.5L76.14 48H24C10.75 48 0 37.25 0 24C0 10.75 10.75 0 24 0H96zM128 464C128 437.5 149.5 416 176 416C202.5 416 224 437.5 224 464C224 490.5 202.5 512 176 512C149.5 512 128 490.5 128 464zM512 464C512 490.5 490.5 512 464 512C437.5 512 416 490.5 416 464C416 437.5 437.5 416 464 416C490.5 416 512 437.5 512 464z" />
+                            </svg>
+
+                            <div
+                                className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+                            >
+                                {cartQuantity}
+                            </div>
+                        </button>
+                    </div>
+
+                </div>
+                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Store</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+
+export default NavBar
