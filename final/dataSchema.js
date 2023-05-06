efaa3d054a172f2a259e85c4477d5ef010b257c0
mtgnoah <noahpravecek@gmail.com>
2023-05-05 19:18:28 -0500

    started on mongodb integration

--- final/dataSchema.js
+++ final/dataSchema.js
@@ -1,11 +1,38 @@
 const mongoose = require('mongoose');
 
-const ReactFormDataSchema = new mongoose.Schema({
-    quantity: { type: Number },
-},
-    {
-        collection: "finalCatalog"
-    })
+// const CartItemDataSchema = new mongoose.Schema({
+//     id: { type: Number },
+//     quantity: { type: Number },
+// },
+//     {
+//         collection: "cartItemCatalog"
+//     })
 
-const Order = mongoose.model('Order', ReactFormDataSchema)
-module.exports = Order
+// const CartItem = mongoose.model('CartItem', CartItemDataSchema)
+
+const cartSchema = new mongoose.Schema({
+    status: { type: String },
+    items: [{
+        itemId: {
+            type: Number,
+        },
+        name: String,
+        quantity: {
+            type: Number,
+            min: 1,
+            default: 1
+        },
+        price: Number
+    }],
+    bill: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+}, {
+    collection: "cartCatalog"
+})
+
+
+const Cart = mongoose.model('Cart', cartSchema)
+module.exports = Cart
--- final/index.js
+++ final/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const Order = require("./dataSchema.js");
+const Cart = require("./dataSchema.js");
 app.use(express.json());
 app.use(cors());
 
@@ -23,23 +23,23 @@ app.listen(port, () => {
 
 app.post("/api/cart", async (req, resp) => {
     query = {};
-    const formData = new Order({
-        quantity: 0,
+    const formData = new Cart({
+        status: "active",
     });
     try {
-        const result = await Order.create(formData);
+        const result = await Cart.create(formData);
         const messageResponse = { "result": result._id };
         resp.send(JSON.stringify(messageResponse));
     }
     catch (err) {
-        console.log("Error while adding a new Order");
+        console.log("Error while adding a new Cart");
     }
     //console.log(allProducts);
 });
 app.get("/:id", async (req, resp) => {
     const id = req.params.id;
     const query = { _id: id };
-    const oneProduct = await Order.findOne(query);
+    const oneProduct = await Cart.findOne(query);
     console.log(oneProduct);
     resp.send(oneProduct);
 })
@@ -47,13 +47,13 @@ app.post("/delete/:id", async (req, res) => {
     const id = req.params.id;
     const query = { _id: id };
     try {
-        const oneProduct = await Order.findOneAndDelete(query);
+        const oneProduct = await Cart.findOneAndDelete(query);
         console.log(oneProduct);
-        const messageResponse = { message: `Order ${id} deleted correctly` };
+        const messageResponse = { message: `Cart ${id} deleted correctly` };
         res.send(JSON.stringify(messageResponse));
     }
     catch (err) {
-        console.log("Error while deleting a Order");
+        console.log("Error while deleting a Cart");
     }
 })
 
@@ -62,18 +62,18 @@ app.post("/updateQuantity", async (req, res) => {
     const query = { _id: id };
     const pquantity = req.body.quantity;
 
-    const formData = new Order({
+    const formData = new Cart({
         _id: id,
         quantity: pquantity
     });
     try {
-        const oneProduct = await Order.findOneAndUpdate(query, formData);
+        const oneProduct = await Cart.findOneAndUpdate(query, formData);
         console.log(oneProduct);
-        const messageResponse = { message: `Order updated correctly` };
+        const messageResponse = { message: `Cart updated correctly` };
         res.send(JSON.stringify(messageResponse));
     }
     catch (err) {
-        console.log("Error while updating a Order");
+        console.log("Error while updating a Cart");
     }
 })
 
@@ -96,5 +96,126 @@ app.post("/insert", async (req, res) => {
     }
     catch (err) {
         console.log("Error while adding a new Order");
+    }
+})
+
+
+router.get("/cart/:id", async (req, res) => {
+    const cartId = req.params.id;
+
+
+    try {
+        const cart = await Cart.findOne({ _id: cartId });
+        if (cart && cart.items.length >= 0) {
+            res.status(200).send(cart);
+        } else {
+            res.send(null);
+        }
+    } catch (error) {
+        res.status(500).send();
+    }
+});
+
+//add cart
+router.post("/cart/increaseQuantity", async (req, res) => {
+    const { itemId, price, name, quantity, cartId } = req.body;
+
+    try {
+        const cart = await Cart.findOne({ _id: cartId });
+        //const item = await CartItem.findOne({ _id: itemId });
+
+        if (!item) {
+            res.status(404).send({ message: "item not found" });
+            return;
+        }
+
+        //If cart already exists for user,
+        if (cart) {
+            const itemIndex = cart.items.findIndex((item) => item.itemId == itemId);
+            //check if product exists or not
+
+            if (itemIndex > -1) {
+                let product = cart.items[itemIndex];
+                product.quantity += quantity;
+
+                cart.bill = cart.items.reduce((acc, curr) => {
+                    return acc + curr.quantity * curr.price;
+                }, 0)
+
+                cart.items[itemIndex] = product;
+                await cart.save();
+                res.status(200).send(cart);
+            } else {
+                cart.items.push({ itemId, name, quantity, price });
+                cart.bill = cart.items.reduce((acc, curr) => {
+                    return acc + curr.quantity * curr.price;
+                }, 0)
+
+                await cart.save();
+                res.status(200).send(cart);
+            }
+        } else {
+            //no cart exists, create one
+            const newCart = await Cart.create({
+                owner,
+                items: [{ itemId, name, quantity, price }],
+                bill: quantity * price,
+            });
+            return res.status(201).send(newCart);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("something went wrong");
+    }
+});
+
+//delete item in cart
+
+router.delete("/cart/decreaseQuantity", async (req, res) => {
+    const cartId =  req.query.cartId;
+    const itemId = req.query.itemId;
+    try {
+        let cart = await Cart.findOne({ _id: cartId });
+
+        const itemIndex = cart.items.findIndex((item) => item.itemId == itemId);
+
+        if (itemIndex > -1) {
+            let item = cart.items[itemIndex];
+            cart.bill -= item.quantity * item.price;
+            if (cart.bill < 0) {
+                cart.bill = 0
+            }
+            cart.items.splice(itemIndex, 1);
+            cart.bill = cart.items.reduce((acc, curr) => {
+                return acc + curr.quantity * curr.price;
+            }, 0)
+            cart = await cart.save();
+
+            res.status(200).send(cart);
+        } else {
+            res.status(404).send("item not found");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(400).send();
+    }
+});
+
+app.post('/checkout', async (req, res) => {
+    try {
+        let cartId = req.body.cartId
+
+        //find cart by id and then just deletes it
+        let cart = await Cart.findOne({ _id: cartId })
+        if (cart) {
+            const data = await Cart.findByIdAndDelete({ _id: cart.id })
+            return res.status(201).send({ status: 'Payment successful' })
+
+        } else {
+            res.status(400).send('No cart found')
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(400).send('invalid request')
     }
 })
